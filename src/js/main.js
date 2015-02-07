0a10e87dd6c8f5de30aa2fbdf06f23e9f86f351e
Martin Indra <martin.indra@mgn.cz>
2015-02-07 13:14:35 +0100

    Issue #1 > create elementary application structure

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 Compeet Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+(function() {
+    var module = angular.module("compeet.Main",
+            [
+                "ngRoute"
+            ]);
+
+    module.config(["$routeProvider", router]);
+    module.controller("compeet.Main.Controller", [
+        controller
+    ]);
+
+    function router($routeProvider) {
+        var PARTIALS = "partials/";
+
+        $routeProvider.
+            when(
+                "/home", {
+                templateUrl: PARTIALS + "home/home.html",
+                controller: "compeet.home.Home.Controller"
+            }).
+            otherwise({
+                redirectTo: "/home"
+            });
+
+    }
+
+    function controller() {
+
+    }
+})();
